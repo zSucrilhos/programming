aa017bdfa21a547ed86a7e0eb8deb8e2ce6845b3
zSucrilhos <e.cesar88@gmail.com>
2018-11-16 16:02:03 -0200

    Initial commit

---
+++ cpp/projects/hello-world/hello-world.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	int numero;
+
+	cout << "Digite alguma coisa: " << endl;
+	cin >> numero;
+	cout << numero << endl;
+
+	system("PAUSE");
+
+	return 0;
+
+}
