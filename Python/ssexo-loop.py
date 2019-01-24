9327a2591f99a3b6e24876a11a7eb7029ea7f41f
zsucrilhos <e.cesar88@gmail.com>
2019-01-24 13:05:49 -0200

    Removed some trash files

--- Python/ssexo-loop.py
+++
@@ -1,5 +0,0 @@
-sexo = input("Digite seu sexo [M/F]: ").strip().upper()[0]
-while sexo not in "MmFf":
-	sexo = input("Dado inválido, por favor, digite seu sexo [M/F]: ").strip().upper()[0]
-print("Dado registrado com sucesso!")
-	
