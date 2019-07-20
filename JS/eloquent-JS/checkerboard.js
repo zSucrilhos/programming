105830f8920fa18f5fd872aa703200700c83e383
zSucrilhos <e.cesar88@gmail.com>
2019-07-19 23:40:23 -0300

    Added caja img

--- JS/eloquent-JS/checkerboard.js
+++ JS/eloquent-JS/checkerboard.js
@@ -1,28 +1,14 @@
  ((rows, columns) => {
-    rows = 4, columns = 5;
-    let positionRows = 0; // Set the positionRows to the number 0
-    let positionColumns = 0; // Set the positionRows to the number 0
-    let array = [];
-
-
-     for (let i = 0; i < columns; i++) {
-         array.push("# ");
+     let string;
+     rows = 4, columns = 5;
+     for (let i = 0; i < rows; i++) {
+         string += "# ";
      }
+     for (let i = 0; i < columns; i++) {
+         string += "\n";
 
-    //while (positionColumns <= columns) {
-        //positionColumns++;
-        //array.push("# ");
-        //if (positionColumns == columns) {
-            //console.log("\n");
-        //}
-    //}
-
-    //// Draw the rows
-    //while (positionRows <= rows) {
-        //positionRows++;
-        //array.push("# ");
-    //} 
-     console.log(array);
+     }
+     console.log(string);
 
 }) ();
 
