7e0823346f61e3da1336adf5af9cd5dd6d2f17ec
zsucrilhos <e.cesar88@gmail.com>
2019-08-01 15:38:37 -0300

    Added new file

---
+++ JS/phone-number-generator.js
@@ -0,0 +1,10 @@
+(function dddGenerator(start=0,end=100) {
+	let array = [];
+	for (let i = start; i < end; i++) {
+		let number1 = (Math.round(Math.random()*9));
+		let number2 = (Math.round(Math.random()*9));
+		let ddd = number1.toString() + number2.toString();
+		array.push(ddd);
+		return console.log(array);
+	}
+})(10, 11);
