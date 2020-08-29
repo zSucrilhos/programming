0e81e65f869b17bcf7e8cd4626b32720db334009
zSucrilhos <e.cesar88@gmail.com>
2020-08-29 18:37:12 -0300

    new changes

--- JS/coursera/react/conFusion/confusion/src/App.js
+++ JS/coursera/react/conFusion/confusion/src/App.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import { Navbar, NavbarBrand } from 'reactstrap';
+import Menu from './components/MenuComponent.js';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div>
+
         <Navbar dark color="primary">
             <div className="container">
                 <NavbarBrand href="/">Ristorante ConFusion</NavbarBrand>
             </div>
         </Navbar>
-    </div>
+        <Menu/>
+
+    </div> // closing App
   );
 }
 
